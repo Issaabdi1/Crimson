e23d995819cdfeed99efcb5e94e41462a0165807
Hanson Zhuang <hansonzhuang@outlook.com>
2024-01-29 17:44:44 +0000

    add admin page config

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import User, Upload
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for users"""
+    list_display = [
+        'username', 'first_name', 'last_name', 'email'
+    ]
+
+
+@admin.register(Upload)
+class UploadAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for uploads"""
+    list_display = [
+        'uploaded_at', 'owner'
+    ]
