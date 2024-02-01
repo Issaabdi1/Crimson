fc81ceb1c8e4a8775a9b8df66d3754d5f74faa11
Hanson Zhuang <hansonzhuang@outlook.com>
2024-02-01 13:01:52 +0000

    Add test cases for the upload model

--- tasks/tests/models/test_upload_model.py
+++ tasks/tests/models/test_upload_model.py
@@ -4,6 +4,8 @@ from unittest.mock import patch, MagicMock
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from tasks.models import User, Upload
+from urllib.parse import quote
+from urllib.request import urlopen
 
 
 class UploadModelTestCase(TestCase):
@@ -17,26 +19,38 @@ class UploadModelTestCase(TestCase):
     def setUp(self):
         """Set up the test case."""
         file_content = b'Test file content'
-        self.mock_file_1 = SimpleUploadedFile('test_file_1.pdf', file_content)
-        self.mock_file_2 = SimpleUploadedFile('test_file_2.pdf', file_content)
-        self.mock_file_3 = SimpleUploadedFile('test_file_3.pdf', file_content)
-
         self.user = User.objects.get(username='@johndoe')
+        self.mock_files = []
+        self.uploads = []
 
-        self.upload_1 = Upload.objects.create(
-            owner=self.user,
-            file=self.mock_file_1
-        )
+        for i in range(1, 4):
+            mock_file = SimpleUploadedFile(f'test_file_{i}.pdf', file_content)
+            upload = Upload.objects.create(owner=self.user, file=mock_file)
+            self.mock_files.append(mock_file)
+            self.uploads.append(upload)
 
-        self.upload_2 = Upload.objects.create(
-            owner=self.user,
-            file=self.mock_file_2
-        )
-
-        self.upload_3 = Upload.objects.create(
-            owner=self.user,
-            file=self.mock_file_3
-        )
+    def tearDown(self):
+        for upload in self.uploads:
+            upload.delete()
 
     def test_uploaded_file_url(self):
         """Test the file's url of the Upload model."""
+        for upload in self.uploads:
+            self.assertEqual(
+                upload.file.url,
+                f'https://mypdfbucket01.s3.amazonaws.com/media/{quote(upload.file.name)}'
+            )
+
+    def test_upload_delete(self):
+        """Test that the upload delete will delete the file in the server"""
+        for upload in self.uploads:
+            file_url = upload.file.url
+            self.assertEqual(urlopen(file_url).getcode(), 200)
+            upload.delete()
+            self.assertFalse(Upload.objects.filter(pk=upload.pk).exists())
+            self.assertEqual(urlopen(file_url).getcode(), 404)
+
+    def test_ordering(self):
+        """Test that uploads are ordered by uploaded_at."""
+        uploads = Upload.objects.all()
+        self.assertEqual(list(uploads), self.uploads)
