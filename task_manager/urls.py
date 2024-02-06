432f02aee2f93c522fe84b3af2c4938762359d87
“HONGYI <hongyi.tu@kcl.ac.uk>
2024-02-06 20:15:03 +0000

    update rename url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path('share_file/', views.share_file, name='share_file'),
     path('process_notification/', views.process_notification_delete, name='process_notification'),
     path('delete/<int:upload_id>/', views.delete_upload, name='delete_upload'),
+    path('filelist/rename/<int:upload_id>', views.rename_upload, name='rename_upload'),
 
 ]
