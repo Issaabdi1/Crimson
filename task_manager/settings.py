8c8c870679db406035eec08f63ee2fca36eb0092
Samuel Chilakamari <samuel.chilakamari@kcl.ac.uk>
2024-02-08 09:41:13 +0000

    add locallost to allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-&$dln5wpgorppuw&(gintxm573v2ks+zq4o$(4*lapguixf^+2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['TeamCrimson.pythonanywhere.com']
+ALLOWED_HOSTS = ['TeamCrimson.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
