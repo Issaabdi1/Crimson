38cab728d8124e9293cb624331ac6e0648bec208
“HILAL <hilal.sahin@kcl.ac.uk>
2024-02-18 19:20:33 +0000

    fix UnboundLocalError in preferences_view

--- tasks/views/preferences_view.py
+++ tasks/views/preferences_view.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def preferences_view(request):
+    selected_theme = request.user.theme_preference 
 
     if request.method == 'POST':
-        selected_theme = request.POST.get('themeSelection')
-
-        if selected_theme:
-            request.user.theme_preference = selected_theme
+        new_selected_theme = request.POST.get('themeSelection')
+        if new_selected_theme:
+            request.user.theme_preference = new_selected_theme
             request.user.save()
-        else:
-            selected_theme = request.user.theme_preference
+            selected_theme = new_selected_theme  
 
     return render(request, 'preferences.html', {'selected_theme': selected_theme})
