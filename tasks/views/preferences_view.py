4f47283b1c54d8ef4519b7094c35a637fde3318a
“HILAL <hilal.sahin@kcl.ac.uk>
2024-02-18 19:15:32 +0000

    fix theme preference override issue

--- tasks/views/preferences_view.py
+++ tasks/views/preferences_view.py
@@ -4,10 +4,12 @@ from django.http import HttpResponse
 def preferences_view(request):
 
     if request.method == 'POST':
-        selected_theme = request.POST.get('themeSelection', 'default-mode')
-        request.user.theme_preference = selected_theme
-        request.user.save()
+        selected_theme = request.POST.get('themeSelection')
 
-    # If GET or after POST, use user's stored theme preference
-    selected_theme = request.user.theme_preference
+        if selected_theme:
+            request.user.theme_preference = selected_theme
+            request.user.save()
+        else:
+            selected_theme = request.user.theme_preference
+
     return render(request, 'preferences.html', {'selected_theme': selected_theme})
