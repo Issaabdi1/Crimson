f7129decbcb66a1e73bf41d15be571a923607dd7
Hanson Zhuang <hansonzhuang@outlook.com>
2024-03-12 12:40:01 +0000

    fix create team window

--- static/js/popup_window.js
+++ static/js/popup_window.js
@@ -16,11 +16,13 @@ var createTeamPopup = document.getElementById("CreateTeamPopup");
 if (createTeamPopup) {
     createTeamPopup.addEventListener("click", function () {
         document.getElementById("popupContainer").style.display = "block";
-        document.getElementById("sidebar").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
         document.getElementById("sidebar").style.zIndex = "999";
     });
 }
 
 document.getElementById("closePopup").addEventListener("click", function () {
     document.getElementById("popupContainer").style.display = "none";
+    if(document.getElementById("sidebar")) {
+        document.getElementById("sidebar").style.zIndex = "0";
+    }
 });
