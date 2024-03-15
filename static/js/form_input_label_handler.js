c89835e1aabd08dace710068c7f046d19c4cff95
Hanson Zhuang <hansonzhuang@outlook.com>
2024-03-15 11:34:53 +0000

    Finish profile and password pages polishing

--- static/js/form_input_label_handler.js
+++ static/js/form_input_label_handler.js
@@ -1,24 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const inputs = document.querySelectorAll('input');
+    // Change ErrorList Position
+    const errorLists = document.querySelectorAll('.errorlist');
 
-    inputs.forEach(input => {
-        const label = input.previousElementSibling;
+    errorLists.forEach(errorList => {
+        const input = errorList.nextElementSibling;
+        input.insertAdjacentElement('afterend', errorList);
+    });
 
-        input.addEventListener('focus', () => {
-            if (label) {
-                console.log(label.textContent);
+    // Handle the effect of label
+    const mainContainers = document.querySelectorAll('.mainContainer');
+    mainContainers.forEach(container => {
+        const inputs = container.querySelectorAll('input');
+        inputs.forEach(input => {
+            const label = input.previousElementSibling;
+
+            if (label && input.value) {
                 label.style.transform = 'translateY(-1.25rem)';
                 label.style.fontSize = '0.8125rem';
                 label.style.color = 'var(--gray-base)';
             }
-        });
 
-        input.addEventListener('blur', () => {
-            if (label && input.value === '') {
-                label.style.transform = '';
-                label.style.fontSize = '';
-                label.style.color = '';
-            }
+            input.addEventListener('focus', () => {
+                if (label) {
+                    console.log(label.textContent);
+                    label.style.transform = 'translateY(-1.25rem)';
+                    label.style.fontSize = '0.8125rem';
+                    label.style.color = 'var(--gray-base)';
+                }
+            });
+
+            input.addEventListener('blur', () => {
+                if (label && input.value === '') {
+                    label.style.transform = '';
+                    label.style.fontSize = '';
+                    label.style.color = '';
+                }
+            });
         });
-    });
+    })
 });
