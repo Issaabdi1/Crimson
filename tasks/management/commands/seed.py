38dae079812f7d58948928248c0ed79272140ce4
Hanson Zhuang <hansonzhuang@outlook.com>
2024-03-17 12:44:07 +0000

    update seeder to seed shared file associated with notification

--- tasks/management/commands/seed.py
+++ tasks/management/commands/seed.py
@@ -1,12 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.files import File
-from tasks.models import User, Upload, Team, ProfileImage
+from tasks.models import User, Upload, Team, ProfileImage, SharedFiles, Notification
 import pytz
 from faker import Faker
 from random import randint, random
 import os
 import requests
 import time
+from django.utils import timezone
 
 user_fixtures = [
     {'username': '@johndoe', 'email': 'john.doe@example.org', 'first_name': 'John', 'last_name': 'Doe'},
@@ -18,16 +19,22 @@ user_fixtures = [
 class Command(BaseCommand):
     """Build automation command to seed the database."""
 
-    # Seeding User, Upload, Team, ProfileImage, SharedFile
+    # Seeding User, Upload, Team associated with shared-file, ProfileImage, SharedFile associated with notifications
+    # User seeding constants
     USER_COUNT = 20
-    TEAM_COUNT = 30
+    # Upload seeding constants
     UPLOAD_COUNT = 100
-    IMAGE_COUNT = USER_COUNT
-    SHARED_FILE_COUNT = USER_COUNT * 3
     UPLOAD_PER_USER = int(UPLOAD_COUNT / USER_COUNT)  # Remember to make it to be integer
+    # Team seeding constants
+    TEAM_COUNT = 30
     USER_PER_TEAM = 5
     UPLOAD_PER_TEAM = 2 * USER_PER_TEAM
+    # Image seeding constants
+    IMAGE_PER_USER = 2  # image_count = image_per_user * user_count
+    # SharedFile seeding constants
+    SHARED_FILE_PER_USER = 3  # shared_file_count = shared_file_per_user * user_count
     USER_PER_SHARED_FILE = 4
+
     DEFAULT_PASSWORD = 'Password123'
     pdf_url_prefix = 'https://mypdfbucket01.s3.eu-west-2.amazonaws.com/media/seeder_data/pdf/sample_file_'
     image_url_prefix = 'https://mypdfbucket01.s3.eu-west-2.amazonaws.com/media/seeder_data/image/image'
@@ -40,6 +47,8 @@ class Command(BaseCommand):
         self.uploads = Upload.objects.all()
         self.teams = Team.objects.all()
         self.images = ProfileImage.objects.all()
+        self.shared_files = SharedFiles.objects.all()
+        self.notifications = Notification.objects.all()
 
     def handle(self, *args, **options):
         self.create_users()
@@ -58,11 +67,19 @@ class Command(BaseCommand):
             seed_image_success = True
         else:
             print("Upload seeding terminated, since user seeding failed.      ")
+        seed_team_success = False
         if seed_image_success:
             self.create_teams()
             self.teams = Team.objects.all()
+            seed_team_success = True
         else:
             print("Team seeding terminated, since image seeding failed.    ")
+        if seed_team_success:
+            self.create_shared_files()
+            self.shared_files = SharedFiles.objects.all()
+            print("Seeding completed.      ")
+        else:
+            print("SharedFile seeding terminated, since team seeding failed.    ")
 
     def create_users(self):
         self.generate_user_fixtures()
@@ -76,6 +93,9 @@ class Command(BaseCommand):
 
     def create_teams(self):
         self.generate_random_teams()
+
+    def create_shared_files(self):
+        self.generate_random_shared_files()
 
     def generate_user_fixtures(self):
         for data in user_fixtures:
@@ -105,11 +125,12 @@ class Command(BaseCommand):
     def generate_random_images(self):
         start_time = time.time()
         image_count = ProfileImage.objects.count()
-        if image_count < self.IMAGE_COUNT:
+        if image_count < self.IMAGE_PER_USER * self.USER_COUNT:
             for user in self.users:
-                print(f"Seeding image {image_count}/{self.IMAGE_COUNT}", end='\r')
-                self.generate_image(user)
-                image_count = ProfileImage.objects.count()
+                for i in range(0, self.IMAGE_PER_USER):
+                    print(f"Seeding image {image_count}/{self.IMAGE_PER_USER * self.USER_COUNT}", end='\r')
+                    self.generate_image(user)
+                    image_count = ProfileImage.objects.count()
         end_time = time.time()
         print("Image seeding complete.        ")
         print(f"Time taken: {int(end_time - start_time)} seconds.")
@@ -127,6 +148,23 @@ class Command(BaseCommand):
             self.generate_team(users)
             team_count = Team.objects.count()
         print("Team seeding complete.      ")
+
+    def generate_random_shared_files(self):
+        shared_file_count = SharedFiles.objects.count()
+        if shared_file_count < self.SHARED_FILE_PER_USER * self.USER_COUNT:
+            for user in self.users:
+                users_upload = Upload.objects.filter(owner=user)
+                used_upload = []
+                for i in range(0, self.SHARED_FILE_PER_USER):
+                    print(f"Seeding shared file {shared_file_count}/{self.SHARED_FILE_PER_USER * self.USER_COUNT}",
+                          end='\r')
+                    upload = users_upload[randint(0, self.UPLOAD_PER_USER - 1)]
+                    while upload in used_upload:
+                        upload = users_upload[randint(0, self.UPLOAD_PER_USER - 1)]
+                    self.generate_shared_file(user, upload)
+                    used_upload.append(upload)
+                    shared_file_count = SharedFiles.objects.count()
+        print("SharedFile seeding complete.      ")
 
     def generate_user(self):
         first_name = self.faker.first_name()
@@ -152,6 +190,12 @@ class Command(BaseCommand):
         shared_uploads = self.create_team_shared_uploads(users)
         self.try_create_team({'users': users, 'name': name, 'shared_uploads': shared_uploads})
 
+    def generate_shared_file(self, user, upload):
+        shared_by = user
+        shared_file = upload
+        shared_to = self.create_shared_file_shared_to(user)
+        self.try_create_shared_file({'shared_by': shared_by, 'shared_file': shared_file, 'shared_to': shared_to})
+
     def try_create_user(self, data):
         try:
             self.create_user(data)
@@ -173,6 +217,12 @@ class Command(BaseCommand):
     def try_create_team(self, data):
         try:
             self.create_team(data)
+        except:
+            pass
+
+    def try_create_shared_file(self, data):
+        try:
+            self.create_shared_file(data)
         except:
             pass
 
@@ -234,6 +284,21 @@ class Command(BaseCommand):
             team.shared_uploads.add(upload)
         team.save()
 
+    def create_shared_file(self, data):
+        shared_file = SharedFiles.objects.create(
+            shared_file=data['shared_file'],
+            shared_by=data['shared_by']
+        )
+        for user in data['shared_to']:
+            shared_file.shared_to.add(user)
+            Notification.objects.create(
+                shared_file_instance=shared_file,
+                user=user,
+                time_of_notification=timezone.now(),
+                notification_message=f'{data['shared_by'].username} shared a file with you'
+            )
+        shared_file.save()
+
     def create_team_shared_uploads(self, users):
         shared_uploads = []
         for user in users:
@@ -244,6 +309,15 @@ class Command(BaseCommand):
                     upload = users_upload[randint(0, self.UPLOAD_PER_USER - 1)]
                 shared_uploads.append(upload)
         return shared_uploads
+
+    def create_shared_file_shared_to(self, user):
+        shared_to = []
+        for i in range(0, self.USER_PER_SHARED_FILE):
+            user_shared_to = self.users[randint(0, self.USER_COUNT - 1)]
+            while user_shared_to in shared_to or user_shared_to == user:
+                user_shared_to = self.users[randint(0, self.USER_COUNT - 1)]
+            shared_to.append(user_shared_to)
+        return shared_to
 
 
 def create_username(first_name, last_name):
