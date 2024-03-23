21dad9a15cdc5590909d98c8c46c113f1514e41f
Hanson Zhuang <hansonzhuang@outlook.com>
2024-03-23 14:38:47 +0000

    fix drop and drag

--- static/js/upload_file.js
+++ static/js/upload_file.js
@@ -7,9 +7,9 @@
 var closeButtons = document.querySelectorAll('.btn-close');
 
 // Loop through each close button
-closeButtons.forEach(function(button) {
+closeButtons.forEach(function (button) {
     // Add click event listener
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         // Find the parent div and remove it
         var parentDiv = button.closest('.uploaded-file');
         if (parentDiv) {
@@ -22,7 +22,7 @@ closeButtons.forEach(function(button) {
  *  form upload listener
  */
 document.addEventListener('DOMContentLoaded', function () {
-    var fileIcon = document.getElementById( 'file-icon');
+    var fileIcon = document.getElementById('file-icon');
     var successIcon = document.getElementById('success-icon');
     var uploadInstruction = document.getElementById('upload-instruction');
     var successAlert = document.getElementById('success-alert');
@@ -59,51 +59,53 @@ function enableUploadButton() {
     uploadButton.disabled = false;
 }
 
-let dropArea = document.getElementById('drop_zone')
+document.addEventListener('DOMContentLoaded', function () {
+    let dropArea = document.getElementById('drop_zone')
 
-dropArea.addEventListener('dragenter', preventDefaults, false)
-dropArea.addEventListener('dragover', preventDefaults, false)
-dropArea.addEventListener('dragleave', preventDefaults, false)
-dropArea.addEventListener('drop', preventDefaults, false)
+    dropArea.addEventListener('dragenter', preventDefaults, false)
+    dropArea.addEventListener('dragover', preventDefaults, false)
+    dropArea.addEventListener('dragleave', preventDefaults, false)
+    dropArea.addEventListener('drop', preventDefaults, false)
 
-function preventDefaults(e) {
-    e.preventDefault()
-    e.stopPropagation()
-}
-
-dropArea.addEventListener('drop', handleDrop, false)
-
-function handleDrop(e) {
-    e.preventDefault(); // Prevent default browser behavior (opening the file)
-
-    const dt = e.dataTransfer;
-    const files = dt.files;
-
-    if (files.length > 1) {
-        console.warn('Multiple file uploads are not supported yet.');
-        return;
+    function preventDefaults(e) {
+        e.preventDefault()
+        e.stopPropagation()
     }
 
-    const fileInput = document.getElementById('file-input');
+    dropArea.addEventListener('drop', handleDrop, false)
 
-    // Assuming the file input has a single child element (the actual input)
-    fileInput.children[0].files = files; // Assign the dropped file(s) to the file input
+    function handleDrop(e) {
+        e.preventDefault(); // Prevent default browser behavior (opening the file)
 
-    var fileIcon = document.getElementById('file-icon');
-    var successIcon = document.getElementById('success-icon');
-    var uploadInstruction = document.getElementById('upload-instruction');
-    var successAlert = document.getElementById('success-alert');
-    var fileName = document.getElementById('file-name')
+        const dt = e.dataTransfer;
+        const files = dt.files;
 
-    if (fileInput.children[0].files.length > 0) {
-        fileIcon.style.display = 'none';
-        successIcon.style.display = 'block';
-        uploadInstruction.style.display = 'none';
-        successAlert.style.display = 'block';
-        fileName.textContent = files[0].name;
+        if (files.length > 1) {
+            console.warn('Multiple file uploads are not supported yet.');
+            return;
+        }
+
+        const fileInput = document.getElementById('file-input');
+
+        // Assuming the file input has a single child element (the actual input)
+        fileInput.children[0].files = files; // Assign the dropped file(s) to the file input
+
+        var fileIcon = document.getElementById('file-icon');
+        var successIcon = document.getElementById('success-icon');
+        var uploadInstruction = document.getElementById('upload-instruction');
+        var successAlert = document.getElementById('success-alert');
+        var fileName = document.getElementById('file-name')
+
+        if (fileInput.children[0].files.length > 0) {
+            fileIcon.style.display = 'none';
+            successIcon.style.display = 'block';
+            uploadInstruction.style.display = 'none';
+            successAlert.style.display = 'block';
+            fileName.textContent = files[0].name;
+        }
+        console.log(`File dropped: ${fileName}`);
     }
-    console.log(`File dropped: ${fileName}`);
-}
+});
 
 document.getElementById('upload-form').addEventListener('submit', function (event) {
     var progressBar = document.getElementById("success-bar");
@@ -133,6 +135,7 @@ document.getElementById('upload-form').addEventListener('submit', function (even
             setTimeout(updateProgress, interval);
         }
     }
+
     updateProgress();
 
 });
